4af62f21d0160ff9a49d1edb80fafcf3ffdbc4eb
Kevin Fairbanks <kevin@kevinfairbanks.com>
2023-11-07 08:59:14 -0500

    css for latest articles

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ defined( 'ABSPATH' ) || exit;
 /*--------------------------------------------------------------*/
 // Define theme version
 if (!defined('MINIMAL_THEME_VERSION')) {
-    define('MINIMAL_THEME_VERSION', '1.0.10');
+    define('MINIMAL_THEME_VERSION', '1.0.11');
 }
 
 // Define theme directory path
