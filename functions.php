acefae5062f50bed30e80e78e1c174e3faaea2db
development <kevin.fairbanks@leafygroup.io>
2024-06-12 17:49:52 +0000

    Weigh-loss page UI

--- functions.php
+++ functions.php
@@ -18,7 +18,7 @@ defined( 'ABSPATH' ) || exit;
 /*--------------------------------------------------------------*/
 // Define theme version
 if (!defined('MINIMAL_THEME_VERSION')) {
-    define('MINIMAL_THEME_VERSION', '1.1.5');
+    define('MINIMAL_THEME_VERSION', '1.1.6');
 }
 
 // Define theme directory path
